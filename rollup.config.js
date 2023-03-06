6d9ece82b6ab323c0a4d89d57849df2e22a82aea
wareverdud <71446364+wareverdud@users.noreply.github.com>
2023-03-06 22:14:01 +0300

    hw3

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import nodeResolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/bundle.js'
+  },
+  plugins: [typescript(), nodeResolve({browser:true}), terser()]
+}
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+const email: string = 'ru.khakimov@innopolis.university'
+const right: HTMLDivElement = document.getElementById('right') as HTMLDivElement
+
+async function getId(): Promise<number> {
+  const queryParams: URLSearchParams = new URLSearchParams({email: email})
+  const idResponse: Response = await fetch('https://fwd.innopolis.app/api/hw2?' +
+      queryParams.toString())
+  return await idResponse.json()
+}
+
+interface Image {
+  day: string
+  month: string
+  year: string
+  alt: string
+  img: string
+  title: string
+}
+
+async function getImage(): Promise<Image> {
+  const id: number = await getId()
+  const queryParams: URLSearchParams = new URLSearchParams({num: id.toString()})
+  const imageResponse: Response = await fetch('https://getxkcd.vercel.app/api/comic?' +
+      queryParams.toString())
+  return await imageResponse.json()
+}
+
+async function appendImage() {
+  const div: HTMLDivElement = document.createElement('div')
+  right.appendChild(div)
+
+  const imageJson: Image = await getImage()
+
+  const titleParagraph: HTMLParagraphElement = document.createElement('p')
+  titleParagraph.innerText = 'Title: ' + imageJson.title
+  div.appendChild(titleParagraph)
+
+  const dateParagraph: HTMLParagraphElement = document.createElement('p')
+  dateParagraph.innerText = `Date: ${imageJson.day}.${imageJson.month}.${imageJson.year}`
+  div.appendChild(dateParagraph)
+
+  const image: HTMLImageElement = document.createElement('img')
+  image.src = imageJson.img
+  image.alt = imageJson.alt
+  image.title = imageJson.title
+  image.className = 'generated-images'
+  div.appendChild(image)
+}
+
+window.addEventListener('load', appendImage)
