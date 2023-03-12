7c38beb4affdc78b413410c6555c3e625386e080
wareverdud <71446364+wareverdud@users.noreply.github.com>
2023-03-13 02:11:56 +0300

    rewritten in svelte

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Footer from './lib/Footer.svelte'
+  import Header from './lib/Header.svelte'
+  import Bio from './lib/Bio.svelte'
+  import Projects from './lib/Projects.svelte'
+  import Comic from './lib/Comic.svelte'
+</script>
+
+<main>
+  <Header />
+  <Bio />
+  <div class="container">
+    <Projects />
+    <Comic />
+  </div>
+  <Footer />
+</main>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: 50% 50%;
+  }
+</style>
